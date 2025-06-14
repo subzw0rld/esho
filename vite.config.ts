6e51f3c866393e7b52865b1ad2fce4e0b6340ca7
Subroto Mukherjee <subrotom.mukherjee@borngroup.com>
2025-06-14 17:22:05 +0530

    reconfigured vite for multi-page support and made folders for about and faq

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import tailwindcss from '@tailwindcss/vite';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
     plugins: [
       tailwindcss(),
     ],
+    build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about/index.html'),
+        faq: resolve(__dirname, 'faq/index.html'),
+      },
+    },
+  },
   });
